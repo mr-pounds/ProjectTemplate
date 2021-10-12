5bac8a99384c1b6465e59e43e9617438b058fe7c
17681762762 <740870608@qq.com>
2021-10-12 12:09:23 +0800

    2

---
+++ {{cookiecutter.project_slug}}/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   main.py
+@Time    :   2021-10-12 12:08:21
+@Author  :   {{cookiecutter.full_name}}
+@Version :   1.0
+@Contact :   {{cookiecutter.email}}
+@Desc    :   None
+'''
+
+# here put the import lib
