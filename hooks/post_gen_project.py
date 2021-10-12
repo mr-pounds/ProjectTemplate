bb6fc2e71c63a82d780b2666df791d8cafee69ef
17681762762 <740870608@qq.com>
2021-10-12 12:24:48 +0800

    V1.0

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,26 +10,9 @@
 '''
 
 # here put the import lib
-import os
-
-PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 PROJECT_NAME = "{{cookiecutter.project_slug}}"
 
-def remove_file(filepath):
-    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
-
 if __name__ == '__main__':
-
-    if '{{ cookiecutter.create_author_file }}' != 'y':
-        remove_file('AUTHORS.rst')
-        remove_file('docs/authors.rst')
-
-    if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
-        remove_file(cli_file)
-
-    if 'Not open source' == '{{ cookiecutter.open_source_license }}':
-        remove_file('LICENSE')
 
     # Enter project directory
     print("""项目创建成功，请继续执行以下操作完成项目创建：
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# here put the import lib
