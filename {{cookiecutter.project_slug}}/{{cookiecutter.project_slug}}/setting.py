7e91f71f8104ee785687d6275f84d6222f667381
17681762762 <740870608@qq.com>
2021-10-22 20:28:54 +0800

    fixed:yaml的使用

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/setting.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/setting.py
@@ -19,4 +19,4 @@ conf_path = os.path.join(project_path, "conf.yaml")
 
 # 读取项目设置
 with open(conf_path, "r", encoding="utf-8") as f:
-    conf = yaml.load(f.read())
+    conf = yaml.load(f.read(), Loader=yaml.FullLoader)
