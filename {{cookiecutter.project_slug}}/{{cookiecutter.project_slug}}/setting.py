e971442f2972df330489114beca77b588bd6c7e3
mr-pounds <740870608@qq.com>
2024-07-01 09:55:28 +0800

    feat: 引入 dotenv 替换掉 yaml 库

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/setting.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/setting.py
@@ -4,30 +4,30 @@ Version      : 1.0.1
 Author       : zzz
 Date         : 2022-01-11 12:13:42
 LastEditors  : zzz
-LastEditTime : 2022-07-20 16:36:48
+LastEditTime : 2024-07-01 09:42:51
 """
-import os
 
-import yaml
+import configparser
+import os
 
 # 项目根目录
 project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # 数据库文件地址
-DB_DIR = os.path.join(project_path, "db")
+# DB_DIR = os.path.join(project_path, "db")
 
 # 日志文件地址
-LOG_DIR = os.path.join(project_path, "log")
+# LOG_DIR = os.path.join(project_path, "log")
 
 # 其他文件地址
-DOC_DIR = os.path.join(project_path, "doc")
+# DOC_DIR = os.path.join(project_path, "doc")
 
 # 静态或模板文档地址
-TEMPLATE = os.path.join(project_path, "template")
-
-# 项目配置文件地址
-conf_path = os.path.join(project_path, "conf.yaml")
+# TEMPLATE = os.path.join(project_path, "template")
 
 # 读取项目设置
-with open(conf_path, "r", encoding="utf-8") as f:
-    conf = yaml.load(f.read(), Loader=yaml.FullLoader)
+conf_path = os.path.join(project_path, "config.ini")
+config = configparser.ConfigParser()
+config.read(conf_path)
+# 获取配置参数
+# params = config.get("default", "params")
